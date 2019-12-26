66c45ec604697fd3c9f13a0a6b1a9d155eb7c70b
scriptandcompile <addtheice@gmail.com>
2019-12-26 15:32:34 -0800

    first pass, dead stupid line count implimented

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,38 @@
-fn main() {
-    println!("Hello, world!");
+use std::env;
+use std::fs;
+use std::path::PathBuf;
+
+fn main() -> std::io::Result<()> {
+    // Get the current directory.
+    let current_path = env::current_dir()?;
+
+    // Find the files in the directory.
+    let path_entries = fs::read_dir(current_path)?;
+
+    let mut files: Vec<PathBuf> = vec![];
+
+    for entry in path_entries {
+        let entry = entry?;
+        let path = entry.path();
+
+        if path.is_file() {
+            let file = path;
+
+            files.push(file);
+        } else if path.is_dir() {
+            // walk the sub directories
+        }
+    }
+
+    // Count the number of lines.
+    for file in &files {
+        let file_name = file.file_name().unwrap();
+        let contents = fs::read_to_string(file)?;
+        let line_count = contents.lines().count();
+
+        // Display the statistics about the code lines.
+        println!("file: {:?} - {:?} lines", file_name, line_count);
+    }
+
+    Ok(())
 }
