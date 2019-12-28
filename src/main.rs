37cf99497c62f270590945108366b0a0d1b5535d
scriptandcompile <addtheice@gmail.com>
2019-12-28 00:30:19 -0800

    recursive file processing now supported

--- src/main.rs
+++ src/main.rs
@@ -1,33 +1,21 @@
+extern crate fs_extra;
+
+use std::error::Error;
 use std::env;
 use std::fs;
-use std::path::PathBuf;
 
-fn main() -> std::io::Result<()> {
+use fs_extra::dir::get_dir_content;
+
+fn main() -> Result<(), Box<dyn Error>> {
     // Get the current directory.
-    let current_path = env::current_dir()?;
+    let current_path = &env::current_dir()?;
 
     // Find the files in the directory.
-    let path_entries = fs::read_dir(current_path)?;
+    let dir_content = get_dir_content(current_path)?;
+    
+    for file_name in dir_content.files {
 
-    let mut files: Vec<PathBuf> = vec![];
-
-    for entry in path_entries {
-        let entry = entry?;
-        let path = entry.path();
-
-        if path.is_file() {
-            let file = path;
-
-            files.push(file);
-        } else if path.is_dir() {
-            // walk the sub directories
-        }
-    }
-
-    // Count the number of lines.
-    for file in &files {
-        let file_name = file.file_name().unwrap();
-        let contents = fs::read_to_string(file)?;
+        let contents = fs::read_to_string(&file_name)?;
         let mut code_line = 0;
         let mut whitespace_line = 0;
 
@@ -38,11 +26,10 @@ fn main() -> std::io::Result<()> {
                 code_line += 1;
             }
         }
-
-        // Display the statistics about the code lines.
+        
         println!(
             "file: {:?} - {:?} code lines, {:?} whitespace lines",
-            file_name, code_line, whitespace_line
+            &file_name, code_line, whitespace_line
         );
     }
 
