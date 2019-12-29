62766e601a106826dc1ed7ed3cabd4615e453729
scriptandcompile <addtheice@gmail.com>
2019-12-28 22:56:48 -0800

    Added basic clap support

--- src/main.rs
+++ src/main.rs
@@ -1,19 +1,21 @@
+extern crate clap;
 extern crate fs_extra;
 
-use std::error::Error;
-use std::path::{PathBuf};
 use std::env;
+use std::error::Error;
 use std::fs;
+use std::path::PathBuf;
 
+use clap::{App, Arg, SubCommand};
 use fs_extra::dir::get_dir_content;
 
 // Need to to fix this in the future to return a collection of
 // search path results and errors. That way we can provide partial
 // results if a file/directory is blocked/missing and a log
-// of unaccessable or non-processable files/directories. 
+// of unaccessable or non-processable files/directories.
 // For now, we simply bail currently on error.
 fn get_file_paths(search_paths: &[PathBuf]) -> Result<Vec<PathBuf>, Box<dyn Error>> {
-    let mut file_paths = vec!();
+    let mut file_paths = vec![];
 
     for search_path in search_paths {
         let dir_content = get_dir_content(search_path)?;
@@ -25,11 +27,8 @@ fn get_file_paths(search_paths: &[PathBuf]) -> Result<Vec<PathBuf>, Box<dyn Erro
     Ok(file_paths)
 }
 
-
 fn process_code_statistics(code_files: &[PathBuf]) -> Result<(), Box<dyn Error>> {
-    
     for file_name in code_files {
-
         let contents = fs::read_to_string(&file_name)?;
         let mut code_line = 0;
         let mut whitespace_line = 0;
@@ -41,7 +40,7 @@ fn process_code_statistics(code_files: &[PathBuf]) -> Result<(), Box<dyn Error>>
                 code_line += 1;
             }
         }
-        
+
         println!(
             "file: {:?} - {:?} code lines, {:?} whitespace lines",
             &file_name, code_line, whitespace_line
@@ -52,12 +51,18 @@ fn process_code_statistics(code_files: &[PathBuf]) -> Result<(), Box<dyn Error>>
 }
 
 fn main() -> Result<(), Box<dyn Error>> {
+    let matches = App::new(env!("CARGO_PKG_NAME"))
+        .version(env!("CARGO_PKG_VERSION"))
+        .author(env!("CARGO_PKG_AUTHORS"))
+        .about(env!("CARGO_PKG_DESCRIPTION"))
+        .get_matches();
+
     // Get the current directory.
     let current_path = env::current_dir()?;
 
     // Find the files in the directory.
     let files = get_file_paths(&[current_path])?;
-    
+
     process_code_statistics(&files)?;
 
     Ok(())
